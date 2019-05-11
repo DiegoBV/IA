479163b45787d5e698f2ee81e3b1f9ee2d29ece6
Diego BV <diego.baratto8@gmail.com>
2019-05-11 22:32:29 +0200

    Player movement 4 testing

---
+++ futbolTest/Assets/NUESTRO/CameraMov.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMov : MonoBehaviour {
+
+    public float sensitivity = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var c = this.transform;
+        c.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        c.Rotate(-Input.GetAxis("Mouse Y") * sensitivity, 0, 0);
+        if (Input.GetMouseButtonDown(0))
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+}
