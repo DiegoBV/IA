74a7ac8aaf8203c404bbe4a05a9e897366590e7c
JuBarea <jubarea@ucm.es>
2019-05-11 15:16:06 +0200

    Goal+Removed Player

---
+++ futbolTest/Assets/NUESTRO/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    /*
+        public GameObject ball;
+        public GameObject NorthGoal;
+        public GameObject SouthGoal;
+    */
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void goal(int team)
+    {
+        print("Team: " + team + " scored a goal");
+    }
+}
