e04071f3ccedca17f57aa7c15649c3505a609028
JuBarea <jubarea@ucm.es>
2019-05-19 17:42:07 +0200

    defence

--- futbolTest/Assets/NUESTRO/Defense.cs
+++ futbolTest/Assets/NUESTRO/Defense.cs
@@ -7,10 +7,9 @@
     using BehaviorDesigner.Runtime;
     using BehaviorDesigner.Runtime.Tasks;
     using Tooltip = BehaviorDesigner.Runtime.Tasks.TooltipAttribute;
+    [TaskCategory("Common")]
     public class Defense : Action
     {
-
-
         [Tooltip("RayoMcQueen en forma de int")]
         public int speed;
         [Tooltip("Target")]
@@ -19,10 +18,19 @@
         public GameObject DefTarget;
         [Tooltip("FlagPoint")]
         public GameObject Flag;
+        [Tooltip("Side of the field")]
+        public bool side;
 
         public override TaskStatus OnUpdate(){
             //Saber si estas a mitad de campo
-
+            if (side) {
+                if(target.transform.position.z >= -12)
+                {
+                    return TaskStatus.Failure;
+                }
+            }else if(target.transform.position.z <= -12) {
+                return TaskStatus.Failure;
+            }
 
 
             Vector3 pos = target.transform.position - DefTarget.transform.position;
@@ -33,7 +41,7 @@
             //Flag.transform.position.y = 8.5;
 
             return TaskStatus.Success;
-        }
 
+        }
     }
 }
