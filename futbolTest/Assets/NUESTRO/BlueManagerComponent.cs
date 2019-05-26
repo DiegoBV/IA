b31b4166dcfa4c1817e971a93d96de57ab837295
JuBarea <jubarea@ucm.es>
2019-05-26 13:48:42 +0200

    Die go

---
+++ futbolTest/Assets/NUESTRO/BlueManagerComponent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+
+public class BlueManagerComponent : MonoBehaviour
+{
+    public GameObject[] team;
+    private bool defense = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void JustScored(int tR, int tB)
+    {
+        if(tR >= tB && !defense)
+        {
+            //cambia a configuracion defensiva
+            foreach(GameObject g in team)
+            {
+                BehaviorDesigner.Runtime.Behavior[] bs = g.GetComponents<BehaviorDesigner.Runtime.Behavior>();
+                BehaviorManager.instance.DisableBehavior(bs[0]);
+                BehaviorManager.instance.EnableBehavior(bs[1]);
+            }
+            defense = true;
+        }
+        else if(defense)
+        {
+            //cambiar a configuracion de ataque
+            //cambia a configuracion defensiva
+            foreach (GameObject g in team)
+            {
+                Behaviour[] bs = g.GetComponents<Behaviour>();
+                bs[0].enabled = !bs[0].enabled;
+                bs[1].enabled = !bs[1].enabled;
+            }
+            defense = false;
+        }
+    }
+}
